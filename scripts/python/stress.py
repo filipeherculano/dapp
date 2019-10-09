7a797cbfc937921b527a2b7a9084df22935b179f
Filipe Herculano <filipeherculanorocha@gmail.com>
2019-10-09 17:23:48 -0300

    IPFS and SWARM tested

--- scripts/python/stress.py
+++ scripts/python/stress.py
@@ -3,12 +3,19 @@ import subprocess
 import sys
 import time
 
-#sizes = [1, 10, 100, 1000, 1000000]
-sizes = [1, 10, 100, 1000, 1000000]
+rounds = 10
+sizes = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
 
 def ipfs():
+    res = []
+
     for size in sizes:
-        for i in range(1):
+        med_store = 0.0
+        med_ret = 0.0
+
+        for i in range(rounds):
+            os.system("ipfs init")
+
             f = open("img.bin","w+b")
             f.write(bytearray(os.urandom(size)))
             f.close()
@@ -16,26 +23,78 @@ def ipfs():
             before = time.time()
             out = subprocess.check_output("ipfs add img.bin", shell=True).split()[1].decode('utf-8')
             after = time.time()
-            print(after - before)
+            med_store = med_store + (after - before)
 
             before = time.time()
-            out = subprocess.check_output("ipfs get " + out, shell=True)
+            os.system("ipfs get " + out)
             after = time.time()
-            print(after - before)
+            med_ret = med_ret + (after - before)
 
+            os.system("sudo rm " + out)
+            os.system("sudo rm -rf ~/.ipfs")
+
+        med_store = med_store / float(rounds)
+        med_ret = med_ret / float(rounds)
+        res.append([med_store, med_ret])
+
+    for i in range(len(res)):
+        print("MEDIAN OF STORE TIME ON SIZE {} IS {}".format(sizes[i], res[i][0]))
+        print("MEDIAN OF RETRIEVE TIME ON SIZE {} IS {}".format(sizes[i], res[i][1]))
+
+def sia():
+    print("NOT IMPLEMENTED")
+
+def swarm():
+    res = []
+
+    for size in sizes:
+        med_store = 0.0
+        med_ret = 0.0
+
+        for i in range(rounds):
+            print("TESTING ROUND {} WITH SIZE {}".format(i, size))
+            os.system("echo 11316652 | swarm init &")
+            time.sleep(2)
+            f = open("img.bin","w+b")
+            f.write(bytearray(os.urandom(size)))
+            f.close()
+
+            before = time.time()
+            out = subprocess.check_output("swarm up img.bin", shell=True).decode('utf-8')
+            after = time.time()
+            med_store = med_store + (after - before)
+
+            before = time.time()
+            os.system("swarm down bzz:/" + out)
+            after = time.time()
+            med_ret = med_ret + (after - before)
+
+            os.system("sudo rm " + out)
+            os.system("kill -9 $(ps aux | grep swarm | grep init | awk '{print $2}')")
+            os.system("sudo rm -rf ~/.ethereum/swarm")
+
+        med_store = med_store / float(rounds)
+        med_ret = med_ret / float(rounds)
+        res.append([med_store, med_ret])
+
+    for i in range(len(res)):
+        print("MEDIAN OF STORE TIME ON SIZE {} IS {}".format(sizes[i], res[i][0]))
+        print("MEDIAN OF RETRIEVE TIME ON SIZE {} IS {}".format(sizes[i], res[i][1]))
+
+def storj():
+    print("NOT IMPLEMENTED")
 
 def main(param):
     if param == "ipfs":
         ipfs()
     elif param == "sia":
-        ipfs()
+        sia()
     elif param == "swarm":
-        ipfs()
+        swarm()
     elif param == "storj":
-        ipfs()
+        storj()
     else :
         print("Invalid Parameter")
 
 if __name__ == "__main__":
-    print(sys.argv[1])
     main(sys.argv[1])
